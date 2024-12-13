bba41d8f100cf3416fa1e3c27c6a13f94654b04b
AtsushiKikuchiKyoto <tosa81st@gmail.com>
2024-12-13 10:58:17 +0900

    1データの取得

--- topic.php
+++ topic.php
@@ -1,3 +1,13 @@
 <?php 
-echo "hello, world!"
+$dbh = new PDO('mysql:host=localhost;dbname=shin_keijiban', "root", "");
+$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // for error
+
+$sql = "SELECT * FROM topic WHERE id = 1";
+$stmt = $dbh->query($sql);
+
+$data = $stmt->fetch(PDO::FETCH_ASSOC); //return 1row
+
+echo $data["topic"];
+echo $data;
+$dbh = null;
 ?>
