a1939c5cbf97890890c6cc4376bf371842db86f2
AtsushiKikuchiKyoto <tosa81st@gmail.com>
2024-12-13 11:02:35 +0900

    エラー処理

--- topic.php
+++ topic.php
@@ -1,13 +1,21 @@
-<?php 
-$dbh = new PDO('mysql:host=localhost;dbname=shin_keijiban', "root", "");
-$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // for error
+<?php
+try{
+  $dbh = new PDO('mysql:host=localhost;dbname=shin_keijiban', "root", "");
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // for error
 
-$sql = "SELECT * FROM topic WHERE id = 1";
-$stmt = $dbh->query($sql);
+  $sql = "SELECT * FROM topic WHERE id = 1";
+  $stmt = $dbh->query($sql);
 
-$data = $stmt->fetch(PDO::FETCH_ASSOC); //return 1row
+  $topic = $stmt->fetch(PDO::FETCH_ASSOC); //return 1row
 
-echo $data["topic"];
-echo $data;
-$dbh = null;
+  if ($topic) {
+    echo $topic["topic"];
+  } else {
+    echo "Topicがありません";
+  }
+  
+  $dbh = null;
+} catch (PDOException $e) {
+  echo $e->getMessage();
+};
 ?>
