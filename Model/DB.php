6f443cf564b360e22b8af29f6289ba37c94e24e6
AtsushiKikuchiKyoto <tosa81st@gmail.com>
2024-12-15 12:04:52 +0900

    環境変数の導入

--- Model/DB.php
+++ Model/DB.php
@@ -8,10 +8,15 @@ class DbConnection {
     public static function getInstance() {
         if (self::$instance === null) {
             try {
-                $dsn = 'mysql:host=localhost;dbname=shin_keijiban';
-                $username = 'root';
-                $password = '';
-                self::$instance = new PDO($dsn, $username, $password);
+                $host = $_ENV['DB_HOST']; 
+                $dbname = $_ENV['DB_NAME'];
+                $user = $_ENV['DB_USER'];
+                $pass = $_ENV['DB_PASS'];
+                // $host = 'localhost';
+                // $dbname = 'shin_keijiban';
+                // $user = 'root';
+                // $pass = '';
+                self::$instance = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
                 self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch (PDOException $e) {
                 die('データベース接続失敗: ' . $e->getMessage());
--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 <!-- http://localhost/phpmyadmin/ -->
 
 <?php
-
+require_once __DIR__ . '/vendor/autoload.php';
+  $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+  $dotenv->load();
 include 'Controller/commentController.php';
 include 'Controller/topicController.php';
 include 'View/pageTop.php';
